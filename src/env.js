a384023b3ea40102be8b6e54e4fe6776b826415c
Luke Yost <lukexyost@gmail.com>
2022-06-15 14:27:23 -0600

    init and added env

--- src/env.js
+++ src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000/' : ''
 export const useSockets = false
-export const domain = 'mickshanny.us.auth0.com'
-export const clientId = 'luwNBP378EfKtXGGgxJj9T2kNZ1A562I'
-export const audience = 'http://Sandbox.com'
+export const domain = 'dev-bawwbekg.us.auth0.com'
+export const clientId = 'C7S064Tv9hUYA501N1IFIlCUVEmJbNnC'
+export const audience = 'https://LukeDev/com'
